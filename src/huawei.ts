a01ad95e52c5908e80e83f337b6fa3e5e070e4a3
Tomas Igrini <tomas.igrini@ness.com>
2023-11-12 14:14:07 +0100

    fix display values

--- src/huawei.ts
+++ src/huawei.ts
@@ -36,11 +36,11 @@ export async function RUN() {
     let s = "";
     const jsn: any = {};
     for (const register of registers) {
-      if (process.env.CONSOLE_DISPLAY === "true") {
+      if (register.display) {
         s += await readData(client, register);
         s += "\n";
+        jsn[register.name] = register.lastValue;
       }
-      jsn[register.name] = register.lastValue;
     }
     clientMqtt.publish(
       process.env.MQTT_TOPIC || "solar/ATTR",
